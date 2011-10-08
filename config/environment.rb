295504dd9aed03b9e0d53d39eeb8e07a38a17dd9
Malligarjunan Sidduraj <malligarjunan@gmail.com>
2011-10-08 20:22:54 +0530

    magent version modified

--- config/environment.rb
+++ config/environment.rb
@@ -42,7 +42,7 @@ Rails::Initializer.run do |config|
   config.gem "geoip"
   config.gem "whatlanguage", :version => "1.0.0"
   config.gem "uuidtools", :version => "2.1.2"
-  config.gem "magent", :version => "0.4.2"
+  config.gem "magent", :version => "0.5.2"
   config.gem "differ", :version => "0.1.1"
   config.gem 'super_exception_notifier', :version => '~> 2.0.0', :lib => 'exception_notifier'
   config.gem "warden"
